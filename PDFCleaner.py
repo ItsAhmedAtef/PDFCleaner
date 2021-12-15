6e2f78b3aa8fecd7cb78c11cc4a4b0740eaab0ec
Ahmed Atef <65677356+ItsAhmedAtef@users.noreply.github.com>
2021-12-15 11:56:21 +0200

    Update PDFCleaner.py

--- PDFCleaner.py
+++ PDFCleaner.py
@@ -6,111 +6,86 @@ import os
 ###################
 
 argv = sys.argv
+allowed_parameters = ["--all","--title","--creator","--author","--producer"]
 
-original_file = open( argv[1], "rb" )
+def clean( file, list_arguments ):
+    file_path, filename = os.path.split(file)
+    new_name = os.path.splitext(filename)[0] + "_cleaned.pdf"
+    new_path_name = os.path.join(file_path, new_name)
+    try:
+        original_file = open( file, "rb")
+    except:
+        sys.exit("Error: Couldn't read \"" + file + "\" file!")
+    try:
+        new_file = open( new_path_name, "wb")
+        print("Created new file " + new_name)
+    except:
+        original_file.close()
+        sys.exit("Error: directory " + file_path + " is not writable!")
+    ##############################
+    found_first_title = False
+    for line in original_file:
+        ##### ##### Title ##### #####
+        if ("--all" in list_arguments or "--title" in list_arguments):
+            if (found_first_title == False):
+                if "/Title(".encode("unicode-escape") in line:
+                    found_first_title = True
+                    Title = line.decode("unicode-escape").split("/Title(")[1].split(")")[0]
+                    title_code = "/Title("+Title+")"
+                    line = line.replace(title_code.encode(), b"/Title()")
+                    print("Title: \"" + Title + "\" (deleted)")
+        ##### ##### Creator ##### #####
+        if ("--all" in list_arguments or "--creator" in list_arguments):
+            if "/Creator(".encode("unicode-escape") in line:
+                Creator = line.decode("unicode-escape").split("/Creator(")[1].split(")")[0]
+                creator_code = "/Creator("+Creator+")"
+                line = line.replace(creator_code.encode(), b"/Creator()")
+                print("Creator: \"" + Creator + "\" (deleted)")
+        ##### ##### Author ##### #####
+        if ("--all" in list_arguments or "--author" in list_arguments):
+            if "/Author(".encode("unicode-escape") in line:
+                Author = line.decode("unicode-escape").split("/Author(")[1].split(")")[0]
+                author_code = "/Author("+Author+")"
+                line = line.replace(author_code.encode(), b"/Author()")
+                print("Author: \"" + Author + "\" (deleted)")
+        ##### ##### Producer ##### #####
+        if ("--all" in list_arguments or "--producer" in list_arguments):
+            if "/Producer(".encode("unicode-escape") in line:
+                Producer = line.decode("unicode-escape").split("/Producer(")[1].split(")")[0]
+                producer_code = "/Producer("+Producer+")"
+                line = line.replace(producer_code.encode(), b"/Producer()")
+                print("Producer: \"" + Producer + "\" (deleted)")
+        new_file.write(line)
+    ##############################
+    original_file.close()
+    new_file.close()
+    sys.exit("Finished")
 
-file_name = os.path.splitext(argv[1])[0]
+def detect_argv(file):
+    del argv[1] # remove this the bigger index first
+    del argv[0]
+    if ( argv ):
+        hold_list_arguments = []
+        for parameter in argv:
+            if (parameter in allowed_parameters):
+                hold_list_arguments.append(parameter)
+            else:
+                print("Warning: Unknown argument \"" + parameter + "\"")
+        if ("--all" in hold_list_arguments):
+            # if --all is provided ? then no need to anything else
+            hold_list_arguments = ["--all"]
+        clean(file, hold_list_arguments)
+    else:
+        clean(file, ["--all"])
 
-file_extension = os.path.splitext(argv[1])[1]
-
-new_name = file_name + "-cleaned" + file_extension
-
-cleaned_file = open( new_name, "wb" )
-
-found_title = True
-
-found_dc_creator = False
-
-for line in original_file:
-
-   ##################################################################
-   if "/Title(".encode('ascii') in line and found_title :
-       Title = str(line).partition("/Title(")[2].split(')')[0]
-       if "\\\\".encode('ascii') in Title.encode("ascii"):
-           Title = Title.encode("ascii").decode('unicode-escape')
-       print( "Title : \033[31m", Title, "\033[0m" )
-       found_title = False
-       if "--all" in argv or "--title" in argv:
-           replace = "/Title(" + Title + ")"
-           line = line.replace(replace.encode("ascii"), b"/Title()")
-           print("\033[41mDeleted\033[0m")
-   ##################################################################
-   if "/Author(".encode('ascii') in line:
-       Author = str(line).partition("/Author(")[2].split(')')[0]
-       if "\\\\".encode('ascii') in Author.encode("ascii"):
-           Author = Author.encode("ascii").decode('unicode-escape')
-       print( "Author : \033[31m", Author, "\033[0m" )
-       if "--all" in argv or "--author" in argv:
-           replace = "/Author(" + Author + ")"
-           line = line.replace(replace.encode("ascii"), b"/Author()")
-           print("\033[41mDeleted\033[0m")
-   ##################################################################
-   if "/Creator(".encode('ascii') in line:
-       Creator = str(line).partition("/Creator(")[2].split(')')[0]
-       if "\\\\".encode('ascii') in Creator.encode("ascii"):
-           Creator = Creator.encode("ascii").decode('unicode-escape')
-       print( "Creator : \033[31m", Creator, "\033[0m" )
-       if "--all" in argv or "--creator" in argv:
-           replace = "/Creator(" + Creator + ")"
-           line = line.replace(replace.encode("ascii"), b"/Creator()")
-           print("\033[41mDeleted\033[0m")
-   ##################################################################
-   if "</dc:creator>".encode('ascii') in line:
-       found_dc_creator = False
-   if found_dc_creator:
-       if "<rdf:li>".encode('ascii') in line:
-           if "</rdf:li>".encode('ascii') in line:
-               Creator = str(line).partition("<rdf:li>")[2].split('</rdf:li>')[0]
-               print( "DC Creator : \033[31m", Creator, "\033[0m" )
-               if "--all" in argv or "--creator" in argv:
-                   replace = "<rdf:li>" + Creator + "</rdf:li>"
-                   line = line.replace(replace.encode("ascii"), b"<rdf:li></rdf:li>")
-                   print("\033[41mDeleted\033[0m")
-   if "<dc:creator>".encode('ascii') in line:
-       if "<rdf:li>".encode('ascii') in line:
-           Creator = str(line).partition("<rdf:li>")[2].split('</rdf:li>')[0]
-           print( "DC Creator : \033[31m", Creator, "\033[0m" )
-           if "--all" in argv or "--creator" in argv:
-               replace = "<rdf:li>" + Creator + "</rdf:li>"
-               line = line.replace(replace.encode("ascii"), b"<rdf:li></rdf:li>")
-               print("\033[41mDeleted\033[0m")
-       elif "<dc:creator>\r\n".encode('ascii') in line:
-           found_dc_creator = True
-   ##################################################################
-   if "/Producer(".encode('ascii') in line:
-       Producer = str(line).partition("/Producer(")[2].split(')')[0]
-       if False in Producer.encode("ascii"):
-           Producer = Producer.encode("ascii").decode('unicode-escape')
-       print( "Producer : \033[31m", Producer, "\033[0m" )
-       if "--all" in argv or "--producer" in argv:
-           replace = "/Producer(" + Producer + ")"
-           line = line.replace(replace.encode("ascii"), b"/Producer()")
-           print("\033[41mDeleted\033[0m")
-   ##################################################################
-   if "<pdf:Producer>".encode('ascii') in line:
-       Producer = str(line).partition("<pdf:Producer>")[2].split('</pdf:Producer>')[0]
-       if "\\\\".encode('ascii') in Producer.encode("ascii"):
-           Producer = Producer.encode("ascii").decode('unicode-escape')
-       print( "XML Producer : \033[31m", Producer, "\033[0m" )
-       if "--all" in argv or "--producer" in argv:
-           replace = "<pdf:Producer>" + Producer + "</pdf:Producer>"
-           line = line.replace(replace.encode("ascii"), b"<pdf:Producer></pdf:Producer>")
-           print("\033[41mDeleted\033[0m")
-   ##################################################################
-   if "/Keywords(".encode('ascii') in line:
-       Keywords = str(line).partition("/Keywords(")[2].split(')')[0]
-       if "\\\\".encode('ascii') in Keywords.encode("ascii"):
-           Keywords = Keywords.encode("ascii").decode('unicode-escape')
-       print( "Keywords : \033[31m", Keywords, "\033[0m" )
-       if "--all" in argv or "--keywords" in argv:
-           replace = "/Keywords(" + Keywords + ")"
-           line = line.replace(replace.encode("ascii"), b"/Keywords()")
-           print("\033[41mDeleted\033[0m")
-   ##################################################################
-
-   cleaned_file.write(line)
-
-print("\033[32mA clean copy created without deleted Tags : \033[0m" + new_name)
-
-original_file.close()
-cleaned_file.close()
+if ( len(argv) > 1 ):
+    file = argv[1]
+    if ( os.path.exists(file) and os.path.isfile(file) ):
+        if (os.path.splitext(file)[1] != ".pdf"):
+            # bad extension
+            sys.exit("Error: \"" + file + "\" is not a PDF file!")
+        detect_argv(file)
+    else:
+        sys.exit("Error: Couldn't read \"" + file + "\" file!")
+else:
+    sys.exit("Error: No arguments provided!")
